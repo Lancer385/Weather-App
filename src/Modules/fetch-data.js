ceb7373b147175f6a8b9966097f9e6cee97936d7
Lancer385 <ahmedkhalaf1234123@gmail.com>
2024-09-23 15:12:44 +0300

    add error handling for bad requests

--- src/Modules/fetch-data.js
+++ src/Modules/fetch-data.js
@@ -1,12 +1,13 @@
 export async function fetchData(location){
     let encodedLocation = encodeURIComponent(location);
-    const promise = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodedLocation}?key=8UJ9SNEGQTUFSD5AL695MA9RX`);
-    const data =  await promise.json();
-    let {address , currentConditions, timezone, days } = data;
-    let extractedData = {};
-    Object.assign(extractedData, {address , currentConditions, timezone, days });
-    console.log(data);
-    console.log(extractedData);
-    return extractedData;
+    const promise = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodedLocation}?unitGroup=uk&key=8UJ9SNEGQTUFSD5AL695MA9RX`);
+    if (promise.ok){
+        const data =  await promise.json();
+        let {address , currentConditions, timezone, days,  } = data;
+        let extractedData = {};
+        Object.assign(extractedData, {address , currentConditions, timezone, days });
+        return extractedData;
+    };
+    return 0;
 };
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import { fetchData } from "./Modules/fetch-data";
+
+const icons = require.context("./icons", false, /\.(png|jpe?g|gif|svg)$/);
+const temperature = document.querySelector("#temperature");
+const location = document.querySelector("#location");
+const condition = document.querySelector("#condition");
+const time = document.querySelector("#time");
 const form = document.createElement("form");
 const userInput = document.createElement("input");
 userInput.type = "text";
 userInput.id = "userInput";
 form.appendChild(userInput);
-document.body.appendChild(form);
+document.querySelector(".container").appendChild(form);
+
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    fetchData(userInput.value);
+    displayData(userInput.value);
 });
 window.fetchData = fetchData;
+
+async function displayData(input){
+    const data = await fetchData(input);
+    if (data !== 0){
+        console.log(data);
+        temperature.textContent = `TEMP:${data.currentConditions.temp}°C`;
+        condition.src = icons(`./${data.currentConditions.icon}.png`);
+        time.textContent = `TIME: ${data.currentConditions.datetime}`;
+    };
+}
+displayData("london");
